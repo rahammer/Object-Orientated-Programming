50ba88dac2c32e10cbde3af1bce61c4a4e30d8c5
Robert <roberthammerschmidt@gmail.com>
2016-10-11 22:50:02 -0700

    Assignment 3

---
+++ Assignment3.java
@@ -0,0 +1,39 @@
+
+Assignment #: 3
+        Name: Robert Hammerschmidt
+   StudentID: 1207364973
+     Lecture: MWF 9:00AM-9:50AM
+
+1. Thou shalt not use a computer to harm other people.
+
+-- Using a computer with integrated hardware that is purposed for injuring
+a person, such as knife-like parts, explosives, flamethrowers, etc. outside
+of a war-time situation has the capabilities to unjustly murder people. Such
+actions should never be allowed.
+
+3. Thou shalt not snoop around in other people's files.
+
+-- Using someone else's computer without permission to find incriminating
+photos and video of said person for exploitation (blackmail) creates
+volatile relations and can destroy reputations on both ends.
+
+4. Thou shalt not use a computer to steal.
+
+-- Using a computer to input someone else's personal information to send
+applications to credit corporations to acquire credit is stealing said
+someone's credit, which is a huge financial resource and can negate the
+chances of the victim acquiring a loan for a major purchase.
+
+5. Thou shalt not use a computer to bear false witness.
+
+-- Using a computer to impersonate another person to either boost a reputation
+or to destroy anothers tarnishes honesty and can create wrongful accusations to
+honest persons thoughout the internet.
+
+
+6. Thou shalt not use or copy software for which you have not paid.
+
+-- Using a computer to obtain and utilize a copy of an expensive software
+such as Logic Pro X or Microsoft Office Professional without properly purchasing
+said software steals the rights the software provider has to that software and
+takes away the finances the prodvider would normally receive.
